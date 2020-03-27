cd42db79e392e6c5669cfb08919ffda1f05dc5c9
catherine <itscatherinehlee@gmail.com>
2020-03-27 14:04:21 -0400

    sensor senses zombie

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# covid19-apocalypse
+# Covid-19 Apocalpse
+## Scenario
+We have a zombie apocalpse thanks to Covid-19. Once a person turns into a zombie they become highly fragile like soap bubbles, and any sufficient disturbance (shock, light, sound, etc) will make them pop. Once they burst, their virus spreads everywhere in the immediate vicinity, and if the virus lands on you, you become a zombie.
+
+Here is my HQ station that can both kill these covid19 zombies, keep me safe, and still allow me so sip on apple juice while I watch from a distance. 
+
+## Challenges that I faced/overcame:
+### No Resources
+I had to deal with limited resources that I already had in my house.
+
+### No Soldering
+I never thought I would have the urge to go buy a soldering iron but because Covid-19 close down GaTech's makerspaces as well as commerical makerspaces, I had to suck it up and use electrical tape (which I had to drive to my mom's house and then steal it from her). 
--- final_project.ino
+++ final_project.ino
@@ -1,1 +1,42 @@
+int haveZombies = 0;
+
+const int trigPin = 2;
+const int echoPin = 3;
+const int ledPin = 8;
+
+long duration;
+int distance;
+
+void setup()
+{
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600); //begin serial communication 
+}
+
+void loop()
+{
+  // Clears the trigPin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2); // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW); // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  // Calculating the distance
+  distance= duration*0.034/2; // Prints the distance on the Serial Monitor
+  if (distance < 100) {
+    Serial.println("IDENTIFIED: ZOMBIE");
+    haveZombies = 1;
+  } else {
+    haveZombies = 0;
+  }
+  if (haveZombies == 1) {
+    digitalWrite(ledPin, HIGH);
+  } else {
+    digitalWrite(ledPin, LOW);
+  }
+
+}
 
